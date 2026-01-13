a2b92d3f35f9ee59a5b0686fcb43fd7ce72d7986
Thao Pham <thachthao.6996@gmail.com>
2026-01-13 18:25:26 +0700

    added test for dropdown list

--- tests/dropdown-list.spec.ts
+++ tests/dropdown-list.spec.ts
@@ -1,15 +1,57 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Locator } from '@playwright/test';
 
-test.describe('Broken Images', () => {
-    test.beforeEach(async ({ page }) => {
-        await page.goto('/dropdown', { 
-            waitUntil: 'commit' 
-        });
-        await page.getByRole('heading', { name: 'Dropdown List'}).waitFor();
+let dropdown: Locator;
+let option1: Locator;
+let option2: Locator;
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/dropdown', {
+        waitUntil: 'domcontentloaded'
     });
 
-    test('page loads', async ({ page }) => {
-        await expect(page).toHaveURL('/dropdown');
-        await expect(page.getByRole('heading', { name: 'Dropdown List' })).toBeVisible();
+    dropdown = page.locator('#dropdown');
+    option1 = page.getByRole('option', { name: 'Option 1' });
+    option2 = page.getByRole('option', { name: 'Option 2' });
+
+    await expect(dropdown).toBeVisible();
+    await expect(dropdown).toHaveValue('');
+})
+
+test.describe('basic tests', () => {
+    test('should select option', async ({ page }) => {
+        await dropdown.selectOption('Option 1');
+        await expect(dropdown).toHaveValue('1');
+        
+        await dropdown.selectOption('Option 2');
+        await expect(dropdown).toHaveValue('2');
+
+    })
+})
+
+test.describe('edge cases', () => {
+    test('should handle rapid selection', async ({ page }) => {
+        let nextValue = '1'; 
+        await dropdown.selectOption(nextValue);
+
+        for (let i = 0; i < 10; i++) {
+            nextValue = nextValue === '1' ? '2' : '1';
+
+            await dropdown.selectOption(nextValue);
+            await expect(dropdown).toHaveValue(nextValue);
+        }
     });
+
+    test('should not select the disabled default option', async ({ page }) => {
+        
+    })
+})
+
+test.describe('accessibility and keyboard navigation', () => {
+    test('should be selectable via keyboard (Tab and Enter', async ({ page }) => {
+
+    })
+})
+
+test.describe('visual UI tests tests', () => {
+    
 })
